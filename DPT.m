810e7bd7e0ede7fa83123121bde9978fb5e3e295
Alli4 <villagomezadriana2016@gmail.com>
2020-02-29 13:57:01 -0800

    updated Cls to have nn

--- DPT.m
+++ DPT.m
@@ -1,4 +1,4 @@
-function[Wfilters,CDiw,CDit,CDi,tdivc,Q,K,Cf,CDmisc,CDleak,CDprot,CDo,CD,q,D,Di,Do,Tr,np,Pav,Tav,Pr] = DPT(PE,CL,W,Swet,Sref,Aw,Sw,At,St,e,t,c,Y,CLw,CLt,xdivc,h,V)
+function[Wfilters,CDiw,CDit,CDi,tdivc,Q,K,Cf,CDmisc,CDleak,CDprot,CDo,CD,q,D,Di,Do,Tr,np,Pav,Tav,Pr] = DPT(n,PE,CL,W,Swet,Sref,Aw,Sw,At,St,e,t,c,Y,CLw,CLt,xdivc,h,V)
 %Use length of aircraft for c
 
 %%%154A Drag Block
@@ -28,28 +28,25 @@ function[Wfilters,CDiw,CDit,CDi,tdivc,Q,K,Cf,CDmisc,CDleak,CDprot,CDo,CD,q,D,Di,
     v = mew/rho;     %kinematic viscosity (m^2/s)
     Re = V.*c/v;       %reynold's number
     q = 0.5*rho*Swet*(V.^2);
+    m = length(V);
 
-%%    
+%% 
+for nn = 1:n
 %%%Calculating Induced Drag Coefficient
-    CDiw = (CLw.^2)/pi/Aw/e;      % CLw = coef. of lift of wing
-    CDit = (CLt.^2)/pi/At/e;      % CLt = coef. of lift of tail 
+    CDiw = (CLw([nn],:).^2)/pi/Aw/e;      % CLw = coef. of lift of wing
+    CDit = (CLt([nn],:).^2)/pi/At/e;      % CLt = coef. of lift of tail 
     %%%Total Induced Drag Coefficient, CDi
     CDi = CDiw + (St/Sw)*CDit;
-    
 %%%Calculating Parasitic Drag Coefficient
     %Form Factor, K
     tdivc = t/c;        %thickness to chord ratio
     K = (1 + (0.6/xdivc)*tdivc + 100*(tdivc^4))*1.34*(M.^0.18)*((cos(Y))^0.28);
-    
     %Interference Factor, Q
     Q = 1;      %fuselages and wings assumed negligible interference
-    
     %Flat Plate Skin Friction, Cf
     Cf = 0.455./((log(Re).^2.58).*(1 + 0.144*M.^2).^0.65);
-    
     %Miscellaneous Drag, CDmisc (due to flaps or unretracted landing gear)
     CDmisc = 0;
-    
     %Leakage Drag, CDleak (due to 'inhalation' and 'exhalation' of filtered air)
     intakearea = 0.3302^2;  %intake area in m^2
     A = intakearea;
@@ -73,36 +70,31 @@ function[Wfilters,CDiw,CDit,CDi,tdivc,Q,K,Cf,CDmisc,CDleak,CDprot,CDo,CD,q,D,Di,
       ModNum = 26;    %Num of modules needed to filter 10^6 cf2h based on modules capable of 330cfm
 %     ModuleSA = SingleModuleSA*ModNum;   %surface area of all filters in the body (m^2)
 %     CDleak = CDleakpercubicmeter*ModuleSA;  %leakage drag for all modules
-      Wprefilter = 1.2224;
-      Wcarbonfilter = 2.5;
-      Whyperhepafilter = 2.1044;
-      Wfilters = ModNum*(Wprefilter+Wprefilter+Whyperhepafilter); %weight of all filters in kg
-    
+    Wprefilter = 1.2224;
+    Wcarbonfilter = 2.5;
+    Whyperhepafilter = 2.1044;
+    Wfilters = ModNum*(Wprefilter+Wprefilter+Whyperhepafilter); %weight of all filters in kg
     %Protuberance Drag, CDprot (due to things that stick out of the body,
     %like the booms for example)
     CDprot = 0;
-    
     %%%Total Parasitic Drag Coefficient, CDo
     CDo =  K.*Q.*Cf.*(Swet/Sref)+CDmisc+CDprot; 
     CDo = CDo + Rdrag;
-
 %%%Total Drag  
-    CD = CDi + CDo;
+    CD = zeros(n,m);
+    CD([nn],:) = CDi + CDo;
     D = q.*CD;
     Di = q.*CDi;
     Do = q.*CDo;
-    
-%%
 %%%Finding Max L/D
-%     CL = CLw + CLt;
+%   CL([n],:) = CLw([n],:) + CLt([n],:);
     Tr = D;         %thrust required
-    figure
-    hold on
-    plot(CD, CL)
-%     plot(CD, max(diff(CL)), '--')      %max cl/cd is at the tangent that crosses the zero (steepest tangent?
-    legend('CL versus CD', 'Tangent of L/D')
-    hold off
-    
+%     figure
+%     hold on
+%     plot(CD([nn],:), CL([nn],:))
+%     plot(CD([nn],:), max(diff(CL)), '--')      %max cl/cd is at the tangent that crosses the zero (steepest tangent?
+%     legend('CL versus CD', 'Tangent of L/D')
+%     hold off
 %%%Propellor 
     npmin = 0;
     npmax = 0.725;
@@ -112,27 +104,29 @@ function[Wfilters,CDiw,CDit,CDi,tdivc,Q,K,Cf,CDmisc,CDleak,CDprot,CDo,CD,q,D,Di,
     Pav = np.*PE;        %Power available
     Tav = Pav./V;       %thrust available
 %%%Power Required
-    top = 2*(Sref^2).*CD.^2;
-    bottom = 1./(rho*(CL.^3));
+    top = 2*(Sref^2).*CD([nn],:).^2;
+    bottom = 1./(rho*(CL([nn],:).^3));
     right = (W/Sref)^3;
     Pr = sqrt(top.*bottom.*right);     %power required
-    figure
-    hold on
-    plot(V,Pr)
-    xlabel('Velocity, V (m/s)')
-    ylabel('Power Required, Pr (W)')
-    hold off
-    
+%%% Plotting moved to the main
+%     figure
+%     hold on
+%     plot(V,Pr)
+%     xlabel('Velocity, V (m/s)')
+%     ylabel('Power Required, Pr (W)')
+%     hold off
+%%% Plotting moved to the main    
 %%% Plotting Parasitic, Induced, and Total Drag
-    figure
-    hold on
-    plot(V, Di, 'r')
-    plot(V, Do, 'g')
-    plot(V, D, 'b')
-    plot(V, Tav)
-    %%add vertical dashed lines for vmin and vmax (point of intersection)
-    legend('Induced Drag', 'Parasitic Drag', 'Total Drag', 'Thrust Available')
-    xlabel('Velocity, V (m/s)')
-    ylabel('Force, D or T(N)')
-    hold off
+%     figure
+%     hold on
+%     plot(V, Di, 'r')
+%     plot(V, Do, 'g')
+%     plot(V, D, 'b')
+%     plot(V, Tav)
+%     %%add vertical dashed lines for vmin and vmax (point of intersection)
+%     legend('Induced Drag', 'Parasitic Drag', 'Total Drag', 'Thrust Available')
+%     xlabel('Velocity, V (m/s)')
+%     ylabel('Force, D or T(N)')
+%     hold off
+end
 end
