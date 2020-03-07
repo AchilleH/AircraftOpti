bee4b28c91554d6286a5b16a4f66021c0d4b6d6e
highflyinryan <61602588+highflyinryan@users.noreply.github.com>
2020-03-07 07:28:49 -0800

    added coefficients list, all known and currently calculable coefficients are preceded by !

--- it_and_deltae.m
+++ it_and_deltae.m
@@ -13,31 +13,38 @@ epsal = downwash; % downwash effect
 v = Vstall; % speed (for L = W)
 Wtotal = W; % total aircraft weight
 
+for I = 1:length(aoa)
+    for J = 1:length(v)
+
 % preliminary calculations
-q = .5*rho*v.^2; % dynamic pressure
+q = .5*rho*v(J).^2; % dynamic pressure
 VH = -staticmargin*St/Sw; % tail volume ratio
-CLdelta_e = tau*at*St/Sw; % lift-curve slope for elevator
-CMdelta_e = -tau.*at.*VH; % Moment-curve slope for elevator
+CLdelta_e(I,:) = tau*at*St/Sw; % lift-curve slope for elevator
+CMdelta_e(I,:) = -tau.*at.*VH; % Moment-curve slope for elevator
 
 % deterimining i_t from total lift equation on Lecture 10 Slide 251
-i_t = 1./at.*Sw./St.*(Wtotal./(q.*Sw)-(aw+at.*St./Sw.*(1-epsal)).*aoa);
+i_t(I,:) = 1./at.*Sw./St.*(Wtotal./(q.*Sw)-(aw+at.*St./Sw.*(1-epsal)).*aoa(I));
 
 % determining elevator deflection from Lecture 10 Slide 253
 
 % total lift-curve slope
-CLa_tot = aw+at.*St./Sw.*(1-epsal); 
+CLa_tot(I) = aw+at.*St./Sw.*(1-epsal); 
 % total moment-curve slope
-CMa = CLa_tot.*staticmargin;
+CMa(I,:) = CLa_tot(I).*staticmargin;
 % lift-curve slope incidence contribution
-CLi = -at.*St./Sw;
+CLi(I,:) = -at.*St./Sw;
 % moment-curve slope incidence contribution
-CMi = at.*VH;
+CMi(I,:) = at.*VH;
 % total coefficent of lift for plane with incidence
-CL_tot = CLa_tot.*aoa+CLi.*i_t;
+CL_tot(I) = CLa_tot(I,:).*aoa(I)+CLi(I,:).*i_t(I,:);
 % moment coefficient around plane AC
-CMac = 1./CLa_tot.*(CMa.*CLi-CLa_tot.*CMi-CMa.*CL_tot);
+
+CMac(I,:) = 1./CLa_tot(I,:).*(CMa(I,:).*CLi(I,:)-CLa_tot(I,:).*CMi(I,:)-CMa(I,:).*CL_tot(I));
 % moment coefficient (not aoa dependent)
-CM0 = VH.*i_t.*at+CMac;
+CM0(I,:) = VH.*i_t(I,:).*at+CMac(I,:);
 
 % elevator deflection
-delta_e = -(CM0.*CLa_tot+CMa.*CL_tot)./(CLa_tot.*CMdelta_e-CMa.*CLdelta_e);
+delta_e(I,:) = -(CM0(I,:).*CLa_tot(I,:)+CMa(I,:).*CL_tot(I))./(CLa_tot(I,:).*CMdelta_e(I,:)-CMa(I,:).*CLdelta_e(I,:));
+    end
+end
+end
