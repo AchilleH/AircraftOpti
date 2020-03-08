3bd7660453fce1644bfb1f2868edbf2e0e977070
Alli4 <61596194+Alli4@users.noreply.github.com>
2020-03-07 18:30:16 -0800

    Update Prop.m
    updated manually

--- Prop.m
+++ Prop.m
@@ -1,7 +1,7 @@
-function[CS, CP, CT, eta_p, J] = Prop(V, Pmotor, T)
+%function[CS, CP, CT, eta_p, J] = Prop(V, Pmotor, T)
+clear all; clc
 
 % I hope this worked
-
 % gamma = 1.4;
 % Rair = 287;
 rho = 1.18883546; % avg. air density (kg/m^3), range of 400-1700ft (121.92-518.16 m) above sea level
@@ -9,46 +9,67 @@ rho = 1.18883546; % avg. air density (kg/m^3), range of 400-1700ft (121.92-518.1
 % a = sqrt(gamma*Rair*Tair); % speed of sound (m/s)
 % M = V/a; % mach number
 
-%T =         % amount of thrust applied/needed
-%Pmotor =    % power supplied by motor
+T = 268.25;        % amount of thrust applied/needed, Nm
+%Pmotor = 37*10^3;    % power supplied by motor, W
 n = 2975/60; % propeller speed [rev/s]
-eta_prop = 0.9; % propeller efficiency
-
-Pin = Pmotor*eta_prop; % power used by propeller
+%Q = 120; %torque of engine Nm
+%Pin = 2*pi*n*Q; % power used by propeller
 % for proppitch = 10:10:70 % plots for individual pitch angles
 
 i = 1;
-for D = .9:.1:15   % different diameters, basically x-axis
-    
-    J(i) = V/(D*n); % advance ratio
-
-    CT(i) = T/(rho*n^2*D^4); % thrust coefficent
-    CP(i) = Pin/(rho*n^3*D^5); % power coefficent
-    CS(i) = (V^5*rho/(Pin*n^2))^0.2; % speed coefficient
-    eta_p(i) = CT(i)*J(i)/CP(i); % propulsive efficency
+figure
+for D = 2
+    r = D/2; A = pi*r^2;
+for V = 0:40
+    eta_p(i) = (0.5+sqrt(0.25+T/2/rho/A/(V^2)))^(-1);
+    J(i) = V/n/D;
+    beta(i) = atan(V/n/r)*(180/pi);  %pitch angle in degrees
     i = i+1;
-    
-    % refer to NACA report 640 for plot comparison in selecting blade and
-    % pitch type
-    
 end
-    figure(1)
-    plot(J,CT)
-    xlabel('J')
-    ylabel('CT')
-    figure(2)
-    plot(J,CP)
-    xlabel('J')
-    ylabel('CP')
-    figure(3)
-    plot(J,CS)
-    xlabel('J')
-    ylabel('Cs')
-    figure(4)
-    plot(J,eta_p)
-    xlabel('J')
-    ylabel('eta_p')
+hold on 
+plot(J,eta_p)
+xlabel('J')
+ylabel('eta_p')
 end
+
+
+%% stuff ryan did
+% i = 1;
+% V = 30; %speed, m/s
+% for D = .9:.1:15   % different diameters, basically x-axis
+%     
+%     J(i) = V/(D*n); % advance ratio
+%     CT(i) = T/(rho*n^2*D^4); % thrust coefficent
+%     CP(i) = Pin/(rho*n^3*D^5); % power coefficent
+%     CS(i) = (V^5*rho/(Pin*n^2))^0.2; % speed coefficient
+%     eta_p(i) = CT(i)*J(i)/CP(i); % propulsive efficency
+%     i = i+1;
+%     hold on 
+%     plot(J,eta_p(i))
+%     xlabel('J')
+%     ylabel('eta_p')
+%     
+%     % refer to NACA report 640 for plot comparison in selecting blade and
+%     % pitch type
+%     
+% end
+%     figure(1)
+%     plot(J,CT)
+%     xlabel('J')
+%     ylabel('CT')
+%     figure(2)
+%     plot(J,CP)
+%     xlabel('J')
+%     ylabel('CP')
+%     figure(3)
+%     plot(J,CS)
+%     xlabel('J')
+%     ylabel('Cs')
+%     figure(4)
+%     plot(J,eta_p)
+%     xlabel('J')
+%     ylabel('eta_p')
+%end
 
 
 
