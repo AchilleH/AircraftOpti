dcdb68da4bd34d7032cce166cea091bce071dd8e
Alli4 <61596194+Alli4@users.noreply.github.com>
2020-03-07 20:32:33 -0800

    Update Prop.m
    updated to include CT and Cp on same graph as etta

--- Prop.m
+++ Prop.m
@@ -1,5 +1,5 @@
 %function[CS, CP, CT, eta_p, J] = Prop(V, Pmotor, T)
-clear all; clc
+clear all; clc; close all
 
 % I hope this worked
 % gamma = 1.4;
@@ -10,27 +10,37 @@ rho = 1.18883546; % avg. air density (kg/m^3), range of 400-1700ft (121.92-518.1
 % M = V/a; % mach number
 
 T = 268.25;        % amount of thrust applied/needed, Nm
-%Pmotor = 37*10^3;    % power supplied by motor, W
+Pmotor = 37*10^3;    % power supplied by motor, W
 n = 2975/60; % propeller speed [rev/s]
-%Q = 120; %torque of engine Nm
-%Pin = 2*pi*n*Q; % power used by propeller
+%Q = 0:12:120; %torque of engine Nm
 % for proppitch = 10:10:70 % plots for individual pitch angles
 
 i = 1;
 figure
-for D = 2
+ D = 2;
     r = D/2; A = pi*r^2;
-for V = 0:40
+for V = 0:100
     eta_p(i) = (0.5+sqrt(0.25+T/2/rho/A/(V^2)))^(-1);
     J(i) = V/n/D;
     beta(i) = atan(V/n/r)*(180/pi);  %pitch angle in degrees
+    CT(i) = T/(rho*n^2*D^4); % thrust coefficent
+    Pin = Pmotor*eta_p(i); % power used by propeller
+    %Pin = 2*pi*n*Q(i); % power used by propeller
+    CP(i) = Pin/(rho*n^3*D^5); % power coefficent
+    %eta_p(i) = CT(i)*J(i)/CP(i); % propulsive efficency
     i = i+1;
 end
 hold on 
+yyaxis right
 plot(J,eta_p)
-xlabel('J')
 ylabel('eta_p')
-end
+yyaxis left
+plot(J, CT)
+plot(J, CP)
+xlabel('J')
+ylabel('CT, CP')
+legend
+
 
 
 %% stuff ryan did
@@ -70,6 +80,7 @@ end
 %     xlabel('J')
 %     ylabel('eta_p')
 %end
+
 
 
 
